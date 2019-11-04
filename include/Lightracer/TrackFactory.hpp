0903066c56757594a6ae91d41d03733b843aebd6
Mark Oates <markoates0@gmail.com>
2019-11-03 21:55:54 -0500

    Move track segment creation functions into "TrackFactory"

---
+++ include/Lightracer/TrackFactory.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#include "Lightracer/TrackSegment.hpp"
+
+
+
+TrackSegment *create_track_segmentA();
+
+TrackSegment *create_track_segmentB();
+
+TrackSegment *create_track_segmentC();
+
+TrackSegment *create_track_segmentD();
+
+TrackSegment *create_track_segmentE();
+
+TrackSegment *opening_gate();
+
+
+
