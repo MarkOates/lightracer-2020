2e29b9d8e70731baf4678a9c2da19afe935de2c7
Mark Oates <markoates0@gmail.com>
2019-11-06 09:08:13 -0500

    Create "Hud" object

---
+++ src/Lightracer/Hud.cpp
@@ -0,0 +1,146 @@
+
+
+#include "Lightracer/Hud.hpp"
+
+
+#include "AllegroFlare/TimerFormatter.hpp"
+#include "AllegroFlare/Useful.hpp" // for tostring
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_color.h>
+#include <sstream>
+
+
+Hud::Hud(int num_of_segments_in_track, Racer *racer, int num_laps_to_win, FontBin &fonts, int screen_width, Timer &stopwatch, int num_lives)
+   : num_of_segments_in_track(num_of_segments_in_track)
+   , racer(racer)
+   , num_laps_to_win(num_laps_to_win)
+   , fonts(fonts)
+   , screen_width(screen_width)
+   , stopwatch(stopwatch)
+   , num_lives(num_lives)
+{
+}
+
+
+Hud::~Hud()
+{
+}
+
+
+void Hud::draw_hud()
+{
+   ALLEGRO_TRANSFORM transform;
+   al_identity_transform(&transform);
+   al_use_transform(&transform);
+
+   
+  int text_y = 40;
+  int horizontal_screen_padding = 100;
+
+   draw_num_segments(text_y, num_of_segments_in_track);
+
+   draw_num_laps(text_y, (int)racer->lap_time.size()+1, num_laps_to_win);
+   draw_stopwatch(text_y, horizontal_screen_padding);
+
+   draw_health_bar(text_y, horizontal_screen_padding);
+}
+
+
+void Hud::draw_num_segments(int text_y, int num_segments)
+{
+   std::stringstream ss;
+   ss << "SEG " << num_segments;
+
+   std::string string_to_write = ss.str();
+
+   ALLEGRO_FONT *font = fonts["venus_rising_rg.ttf 26"];
+   ALLEGRO_COLOR color = al_color_name("white");
+   al_draw_text(font, color, screen_width - 300 - 235, text_y, ALLEGRO_ALIGN_LEFT, string_to_write.c_str());
+}
+
+
+void Hud::draw_num_laps(int text_y, int current_lap_num, int total_num_laps)
+{
+   std::string string_to_write = "";
+   std::stringstream ss;
+
+   ss << "LAP ";
+   if (current_lap_num < total_num_laps)
+   {
+      ss << current_lap_num << " / " << total_num_laps;
+   }
+   else if (current_lap_num == total_num_laps)
+   {
+      ss << "FIN";
+   }
+   else
+   {
+      ss << "-";
+   }
+   string_to_write = ss.str();
+
+
+   ALLEGRO_FONT *font = fonts["venus_rising_rg.ttf 26"];
+   ALLEGRO_COLOR color = al_color_name("white");
+   al_draw_text(font, color, screen_width - 300 - 300 - 200, text_y, ALLEGRO_ALIGN_LEFT, string_to_write.c_str());
+}
+
+
+void Hud::draw_stopwatch(int text_y, int horizontal_screen_padding)
+{
+   std::string ellapsed_time_str = std::string("TIM ") + TimerFormatter(stopwatch.get_elappsed_time_msec()).format();
+   ALLEGRO_FONT *font = fonts["venus_rising_rg.ttf 26"];
+   ALLEGRO_COLOR color = al_color_name("white");
+
+   //float text_width = al_get_text_width(font, ellapsed_time_str.c_str());
+   std::string the_char = " ";
+   int letter_spacing = 12 * 2;
+   int letters_traversed = 0;
+   for (auto &c : ellapsed_time_str)
+   {
+      the_char[0] = c;
+      al_draw_text(font, color, screen_width - 375 + (letters_traversed * letter_spacing), text_y, ALLEGRO_ALIGN_CENTER, the_char.c_str());
+      letters_traversed++;
+   }
+}   
+
+
+void Hud::draw_health_bar(int text_y, int horizontal_screen_padding)
+{
+   int health_bar_height = 10;
+   ALLEGRO_FONT *font = fonts["venus_rising_rg.ttf 26"];
+
+   std::string lives_string = "LIV " + tostring(num_lives);
+   al_draw_text(font, al_color_name("white"), horizontal_screen_padding, text_y, 0, lives_string.c_str());
+
+   float health_percentage = racer->health/racer->max_health;
+
+   ALLEGRO_COLOR health_bar_color;
+   if (health_percentage > 0.9) health_bar_color = al_color_name("dodgerblue");
+   else if (health_percentage > 0.6) health_bar_color = al_color_name("violet");
+   else if (health_percentage > 0.3) health_bar_color = al_color_name("orange");
+   else health_bar_color = al_color_name("red");
+
+   int lives_text_length = 120;
+   int bar_length = 200;
+   int bar_y = text_y + al_get_font_ascent(font) / 2 + 4;
+   int bar_x = horizontal_screen_padding + lives_text_length + 4;
+
+   al_draw_rounded_rectangle(
+         bar_x - 4,
+         bar_y - 4 - health_bar_height * 0.5,
+         bar_x + 4 + bar_length,
+         bar_y + 4 + health_bar_height * 0.5,
+         4, 4,
+         al_color_name("lightblue"), 2.0);
+
+   al_draw_filled_rectangle(
+         bar_x,
+         bar_y - health_bar_height * 0.5,
+         bar_x + bar_length * health_percentage,
+         bar_y + health_bar_height * 0.5,
+         al_color_name("lightblue"));
+}
+
+
