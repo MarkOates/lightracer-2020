10751c33676281d915728a249ed6942e399cfc95
Mark Oates <mark.oates@shopify.com>
2019-11-06 12:22:45 -0500

    Move Hud functions into private scope

--- include/Lightracer/Hud.hpp
+++ include/Lightracer/Hud.hpp
@@ -17,17 +17,16 @@ private:
    Timer &stopwatch;
    int num_lives;
 
-public:
-   Hud(int num_of_segments_in_track, Racer *racer, int num_laps_to_win, FontBin &fonts, int screen_width, Timer &stopwatch, int num_lives);
-   ~Hud();
-
-   void draw();
-
    void draw_hud();
    void draw_num_segments(int text_y, int num_segments);
    void draw_num_laps(int text_y, int current_lap_num, int total_num_laps);
    void draw_stopwatch(int text_y, int horizontal_screen_padding);
    void draw_health_bar(int text_y, int horizontal_screen_padding);
+public:
+   Hud(int num_of_segments_in_track, Racer *racer, int num_laps_to_win, FontBin &fonts, int screen_width, Timer &stopwatch, int num_lives);
+   ~Hud();
+
+   void draw();
 };
 
 
