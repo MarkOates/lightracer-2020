728c69cecd7049c28762849e8936a9cfcf26d5cd
Mark Oates <markoates0@gmail.com>
2019-11-06 20:22:22 -0500

    Add empty "GLRenderer::draw_gl_projection" with signature

--- include/Lightracer/GLRenderer.hpp
+++ include/Lightracer/GLRenderer.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
 
+#include "Lightracer/Camera3.hpp"
+#include "AllegroFlare/ModelBin.hpp"
+#include <allegro5/allegro.h>
+
+
+class Racer;
+class Track;
+
+
+using AllegroFlare::ModelBin;
+
+
 class GLRenderer
 {
 public:
    GLRenderer();
+
+   void draw_gl_projection(Camera3 &camera3, Racer *racer, ALLEGRO_BITMAP *bitmap, ModelBin &models, Track *track_);
 };
 
 
--- src/Lightracer/GLRenderer.cpp
+++ src/Lightracer/GLRenderer.cpp
@@ -3,7 +3,16 @@
 #include "Lightracer/GLRenderer.hpp"
 
 
+#include "Lightracer/Racer.hpp"
+#include "Lightracer/Track.hpp"
+
+
 GLRenderer::GLRenderer()
+{
+}
+
+
+void GLRenderer::draw_gl_projection(Camera3 &camera3, Racer *racer, ALLEGRO_BITMAP *bitmap, ModelBin &models, Track *track_)
 {
 }
 
