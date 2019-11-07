6307f87c760c492331ac18c9628965fda9935f80
Mark Oates <markoates0@gmail.com>
2019-11-06 19:05:14 -0500

    Use extracted Camera class in main source

--- programs/lightracermax.cpp
+++ programs/lightracermax.cpp
@@ -274,110 +274,8 @@ CheapCamera *camera = nullptr;
 
 
 
-class Camera : public vec2d
-{
-private:
+#include "Lightracer/Camera.hpp"
 
-public:
-   float z;
-
-   //animatable<float> z;
-   float depth;
-   vec2d center_point;
-   float home_z;
-
-   bool keyboard_controls_on;
-
-   Camera()
-   {
-      //_z = 0.0;
-      //z.set(&_z);
-
-      depth = 128;  // the bigger this number, the less apparent the depth effect
-      depth = 512;  // 512 is an accurate balance to where moving right 100px feels
-                    // proportionally the same distance as moving forward 100px
-      home_z = depth;
-      z = home_z;
-      center_point = vec2d(SCREEN_W/2, SCREEN_H/2);
-      keyboard_controls_on = true;
-   }
-
-   ALLEGRO_COLOR foreground_color; 
-
-   inline float get_scale(float object_z)
-   {
-      float distance_from_camera = z - object_z;
-      if (distance_from_camera <= 0) return 0;
-      if (depth <= 0) return 0;
-
-      return depth / distance_from_camera;
-   }
-
-   inline vec2d get_displacement(float object_z)
-   {
-      float scale = get_scale(object_z);
-      return vec2d(-x * scale + SCREEN_HW, -y * scale + SCREEN_HH);
-   }
-
-   //inline vec3d screen_coordinates_to_world(float 
-   
-   
-   //inline vec2d get_coordinate_on_screen(float x, float y, float object_z)
-   //{
-      //return vec2d(x, y) + get_scale(object_z);
-      //return ;
-   //}
-   
-
-   vec2d get_xy_displacement()
-   {
-      return vec2d(-x + SCREEN_HW, -y + SCREEN_HH);
-   }
-
-   /*
-   bool zoom_to(float position=512, float duration=3.0, float (*transition_curve)(float)=&interpolator::linear)
-   {
-      if (z.get_num_active_animations() != 0) return false;
-      if (_z == position) return false;
-      z.animate(_z, position, duration, transition_curve);
-      return true;
-   }
-   */
-
-
-   void draw_crosshair(float _x, float _y, float _z)
-   {
-      float scale = get_scale(_z);
-      float width = 10;
-      al_draw_line((_x-x)*scale - width*scale + center_point.x,
-                  (_y-y)*scale + center_point.y,
-                  (_x-x)*scale + width*scale + center_point.x,
-                  (_y-y)*scale + center_point.y,
-                  al_color_name("red"), 1.0*scale);
-      al_draw_line((_x-x)*scale + center_point.x,
-                  (_y-y)*scale - width*scale + center_point.y,
-                  (_x-x)*scale + center_point.x,
-                  (_y-y)*scale + width*scale + center_point.y,
-                  al_color_name("red"), 1.0*scale);
-   }
-
-   vec2d get_point(float _x, float _y, float _z)
-   {
-      float scale = get_scale(_z);
-      return vec2d((_x-x)*scale + center_point.x,  (_y-y)*scale + center_point.y);
-   }
-
-   void turn_on_controls()
-   {
-      keyboard_controls_on = true;
-   }
-
-   void turn_off_controls()
-   {
-      keyboard_controls_on = false;
-   }
-
-};
 
 
 Camera *good_camera = nullptr;
@@ -1300,7 +1198,7 @@ void init_game()
    racer = new Racer();
 
    camera = new CheapCamera();
-   good_camera = new Camera();
+   good_camera = new Camera(SCREEN_W, SCREEN_H);
 
    fade_out_of_black();
 
