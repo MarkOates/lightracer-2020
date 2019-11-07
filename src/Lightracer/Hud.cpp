51dfbed2606c92aff365f5d4e40682ed2b22c031
Mark Oates <markoates0@gmail.com>
2019-11-06 23:11:01 -0500

    Replace instances of "screen_width - 300" with "anchor_position"

--- src/Lightracer/Hud.cpp
+++ src/Lightracer/Hud.cpp
@@ -48,13 +48,15 @@ void Hud::draw_hud()
    int text_y = 40;
    int horizontal_screen_padding = 100;
 
+   int anchor_position = screen_width - 300;
+
    draw_health_bar(horizontal_screen_padding, text_y);
-   draw_wall_hit_count(screen_width - 300 - 724, text_y);
-   draw_throttle_release_count(screen_width - 300 - 924, text_y);
-   draw_num_laps(screen_width - 300 - 500, text_y, num_laps_to_win);
-   draw_num_segments(screen_width - 300 - 264, text_y, num_of_segments_in_track);
-   draw_count_of_removed_track_segments(screen_width - 300 - 1124, text_y);
-   draw_stopwatch(screen_width - 300 - 75, text_y);
+   draw_wall_hit_count(anchor_position - 724, text_y);
+   draw_throttle_release_count(anchor_position - 924, text_y);
+   draw_num_laps(anchor_position - 500, text_y, num_laps_to_win);
+   draw_num_segments(anchor_position - 264, text_y, num_of_segments_in_track);
+   draw_count_of_removed_track_segments(anchor_position - 1124, text_y);
+   draw_stopwatch(anchor_position - 75, text_y);
 }
 
 
