0daf4cd03f7a310c72d267e988669602837de3f0
Mark Oates <markoates0@gmail.com>
2019-11-06 20:25:19 -0500

    Replace insource Camera3 with new isolated component

--- programs/lightracermax.cpp
+++ programs/lightracermax.cpp
@@ -991,49 +991,8 @@ void game_timer_func(ALLEGRO_EVENT *current_event)
 }
 
 
-class Camera3
-{
-public:
-	vec3d position;
-	vec3d stepout;
-	float spin;
-	float tilt;
-	Camera3()
-		: position(0, 0, 0)
-		, stepout(0, 0, 0)
-		, spin(0)
-		, tilt(0)
-	{}
-	void position_transform(ALLEGRO_TRANSFORM *t)
-	{
-		al_identity_transform(t);
 
-		al_translate_transform_3d(t, stepout.x, stepout.y, stepout.z);
-		al_rotate_transform_3d(t, -1, 0, 0, tilt);
-		al_rotate_transform_3d(t, 0, -1, 0, spin);
-	}
-	void reverse_position_transform(ALLEGRO_TRANSFORM *t)
-	{
-		// note: this is EXACTLY the same as position transform, except the
-		// order of transformations is reversed, and the values are negated
-		al_identity_transform(t);
-
-		al_rotate_transform_3d(t, 0, 1, 0, spin);
-		al_rotate_transform_3d(t, 1, 0, 0, tilt);
-		al_translate_transform_3d(t, -stepout.x, -stepout.y, -stepout.z);
-	}
-	vec3d get_real_position()
-	{
-		vec3d real_position(0, 0, 0);
-		ALLEGRO_TRANSFORM t;
-
-		position_transform(&t);
-		al_transform_coordinates_3d(&t, &real_position.x, &real_position.y, &real_position.z);
-
-		return real_position;
-	}
-};
-
+#include "Lightracer/Camera3.hpp"
 
 
 
