49e8b991850f99fde9cb40953bdd1a6d62790379
Mark Oates <markoates0@gmail.com>
2019-11-06 19:55:27 -0500

    Move utility function up in file

--- programs/lightracermax.cpp
+++ programs/lightracermax.cpp
@@ -181,6 +181,51 @@ void restart_music()
 }
 
 
+std::string get_number_string(int num)
+{
+   switch(num)
+   {
+   case 1:
+      return "one";
+      break;
+   case 2:
+      return "two";
+      break;
+   case 3:
+      return "three";
+      break;
+   case 4:
+      return "four";
+      break;
+   case 5:
+      return "five";
+      break;
+   case 6:
+      return "six";
+      break;
+   case 7:
+      return "seven";
+      break;
+   case 8:
+      return "eight";
+      break;
+   case 9:
+      return "nine";
+      break;
+   case 10:
+      return "ten";
+      break;
+   case 11:
+      return "eleven";
+      break;
+   default: return tostring(num);
+      break;
+   }
+}
+
+
+
+
 
 //int segment_where_player_died = 0;
 
@@ -771,51 +816,6 @@ void DEVELOPER_key_down_func()
    }
 }
 */
-
-
-std::string get_number_string(int num)
-{
-   switch(num)
-   {
-   case 1:
-      return "one";
-      break;
-   case 2:
-      return "two";
-      break;
-   case 3:
-      return "three";
-      break;
-   case 4:
-      return "four";
-      break;
-   case 5:
-      return "five";
-      break;
-   case 6:
-      return "six";
-      break;
-   case 7:
-      return "seven";
-      break;
-   case 8:
-      return "eight";
-      break;
-   case 9:
-      return "nine";
-      break;
-   case 10:
-      return "ten";
-      break;
-   case 11:
-      return "eleven";
-      break;
-   default: return tostring(num);
-      break;
-   }
-}
-
-
 
 
 void main_menu_timer_func(ALLEGRO_EVENT *event)
